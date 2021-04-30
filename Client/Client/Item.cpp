0ef64542f120385960e03eab54cb55d6c5ecf730
Gaston Hugo Frossasco <gaston.frossasco@me.cba.gov.ar>
2021-04-30 19:24:28 -0300

    added npc sprites in minimap

---
+++ Client/Client/Item.cpp
@@ -0,0 +1,30 @@
+// Item.cpp: implementation of the CItem class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#include "Item.h"
+
+//////////////////////////////////////////////////////////////////////
+// Construction/Destruction
+//////////////////////////////////////////////////////////////////////
+
+CItem::CItem()
+{
+	ZeroMemory(m_cName, sizeof(m_cName));
+	m_sSprite = 0;
+	m_sSpriteFrame = 0;
+	m_dwAttribute = NULL;
+	m_sItemSpecEffectValue1 = 0;
+	m_sItemSpecEffectValue2 = 0;
+	m_sItemSpecEffectValue3 = 0;
+
+	m_iClass = 0;
+
+	m_iReqStat = 0;
+	m_iQuantStat = 0;
+}
+
+CItem::~CItem()
+{
+	
+}
